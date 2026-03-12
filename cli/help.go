12758a4ed6db635766066c42d1232dc57372158b
KiamMota <somentekiam@gmail.com>
2026-03-11 22:08:58 -0300

    changing path of help

---
+++ cli/help.go
@@ -0,0 +1,31 @@
+package cli
+
+func Help() {
+	print(`
+
+lpm — lazy plugin manager
+
+usage:  lpm <command> [argument]
+
+commands:
+  install    <github url>   download and create plugin config
+  remove     <plugin>       delete plugin config file
+  able       <plugin>       toggle enabled in plugin config
+  cat        <plugin>       print plugin config
+  edit       <plugin>       open plugin config in $EDITOR
+  edit       <plugin> --nvim   open plugin config in nvim
+  list                      list installed plugins
+  sync                      run :Lazy sync headless
+
+flags:
+  -v, --version             show version
+  -u, --self-update         update lpm
+  -h, --help                show this message
+
+examples:
+  lpm install https://github.com/nvim-telescope/telescope.nvim
+  lpm able telescope
+  lpm cat telescope
+  lpm sync
+`)
+}
